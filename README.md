d878ebd914958352bd272602386a8fa3c90bde3f
mdyousufalibd501-sketch <mdyousufalibd501@gmail.com>
2025-10-05 07:13:30 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+#### YouTube Video Download Bot V2 
+###### This A simple YouTube Video Download Telegram Bot
+
+
+![logo](https://graph.org/file/754b7faa1308a13fc917f.jpg)
+
+
+# Donation
+<b><details><summary>Click To Open Donation detelis</summary>
+
+## Support This Project
+
+Maintaining and improving this project takes time and resources. If you find this repository helpful or use it in your own projects, consider supporting its development. Your contributions—no matter how small—help keep the project alive and evolving.
+
+### Payment Methods
+
+- **UPI / GPay / Paytm / PhonePe**
+- **PayPal** (International)
+- **Credit/Debit Cards** (via PayPal)
+- **Cryptocurrency**: Bitcoin, Tron (TRC20)
+- **Payeer**
+
+### Payment Details
+
+- **UPI ID**: `jobinjustin2002-1@okaxis`  
+- **Bitcoin**: `1DPtf1xkq7aBriCBwDHNydSfzQT2PccFmj`  
+- **Tron (TRC20)**: `THPYzzchV2fssaw5RE4TxL8NxJpfctw46J`  
+- **Payeer**: `P1113797003`  
+- **PayPal**: [Click to Pay](https://www.paypal.me/lisajobin)
+
+### Why Donate?
+
+- To support ongoing development and new features
+- To help cover infrastructure and hosting costs
+- To keep the project open-source and free for everyone
+
+### Contact
+
+If you have any questions or would like to sponsor this project in another way, feel free to reach out:  
+**[@JOBIN_JUSTIN](https://t.me/JOBIN_JUSTIN)** on Telegram.
+</b>
+</details>
+
+
+# How To Deploy
+
+<b><details><summary>Tap To watch</summary>
+
+### Heroku Video
+<a href="https://youtu.be/ms_ApEgb0SA?feature=shared"><img alt="how to create" src="https://img.shields.io/badge/-YouTube-red?style=for-the-badge&logo=youtube&logoColor=white"/></a> 
+
+### Render Video
+<a href="https://youtu.be/A4l6LSPi-lM?feature=shared"><img alt="how to create" src="https://img.shields.io/badge/-YouTube-red?style=for-the-badge&logo=youtube&logoColor=white"/></a>
+
+</b>
+</details>
+
+## Environment Variable
+
+* `APP_ID` Get it From mytelegram.org
+
+* `API_HASH` Get it From mytelegram.org
+
+* `BOT_TOKEN` Get it from [@Botfather](https://t.me/botfather)
+
+* `CHANNEL` Add your forcesub channel id
+
+* `Cookies.txt` Add your youtube cookies
+
+### Features
+
+- Download any youtube videos no age restriction 
+
+- Generate any youtube video thumbnail
+
+- Date and time added use `/date`
+
+- force-sub 
+
+**New features adding soon...**
+### Bot Commands 
+```
+start - Check Bot Online 🔔
+help - How To Use The Bot 🆘
+about - Something About Me 😌
+thumbnail - generate video thumbnail 🌁
+date - check date and time 🕰️
+```
+
+### BUY ME A COFFEE 🥹
+<a href="https://www.buymeacoffee.com/lisakorean" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/arial-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
+
+### My Community Details
+
+
+- YouTube Channel : [Telegram Bots 🤖](https://youtube.com/@NTBOT?feature=shared)
+- Telegram Channel : [NT Bots ❤️‍🩹](https://t.me/NT_BOT_CHANNEL)
+- Telegram Group : [NT Bots Support 🎗️](https://t.me/NT_BOTS_SUPPORT)
+- URL Uploader Bot : [Uploader Bot 🚀](https://t.me/UploadLinkToFileBot)
+- Me : [Lisa 👑](https://t.me/LISA_FAN_LK)
---
+++ Youtube/youtube.py
@@ -0,0 +1,97 @@
+# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/YouTube-Video-Download-Bot
+
+# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/YouTube-Video-Download-Bot
+
+import os
+import logging
+import asyncio
+import yt_dlp
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from Youtube.config import Config
+from Youtube.forcesub import handle_force_subscribe
+
+youtube_dl_username = None  
+youtube_dl_password = None 
+
+@Client.on_message(filters.regex(r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'))
+async def process_youtube_link(client, message):
+    if Config.CHANNEL:
+        fsub = await handle_force_subscribe(client, message)
+        if fsub == 400:
+            return
+    youtube_link = message.text
+    
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Best Quality", callback_data=f"download|best|{youtube_link}")],
+        [InlineKeyboardButton("1080p", callback_data=f"download|1080p|{youtube_link}")],
+        [InlineKeyboardButton("2K", callback_data=f"download|2k|{youtube_link}")],
+        [InlineKeyboardButton("4K", callback_data=f"download|4k|{youtube_link}")],
+        [InlineKeyboardButton("Medium Quality", callback_data=f"download|medium|{youtube_link}")],
+        [InlineKeyboardButton("Low Quality", callback_data=f"download|low|{youtube_link}")]
+    ])
+    
+    await message.reply_text("**Getting Available Formats**", reply_markup=keyboard)
+
+@Client.on_callback_query(filters.regex(r'^download\|'))
+async def handle_download_button(client, callback_query):
+    quality, youtube_link = callback_query.data.split('|')[1:]
+
+    quality_format = {
+        'best': 'best',
+        '1080p': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
+        '2k': 'bestvideo[height<=1440]+bestaudio/best[height<=1440]',
+        '4k': 'bestvideo[height<=2160]+bestaudio/best[height<=2160]',
+        'medium': 'best[height<=480]',
+        'low': 'best[height<=360]'
+    }.get(quality, 'best')
+
+    try:
+        await callback_query.message.edit_text("**Downloading video...**")
+
+        ydl_opts = {
+            'format': quality_format,
+            'outtmpl': 'downloaded_video_%(id)s.%(ext)s',
+            'merge_output_format': 'mp4',
+            'progress_hooks': [lambda d: print(f"[yt_dlp] {d.get('status')}")],
+            'cookiefile': 'cookies.txt'
+        }
+
+        if Config.HTTP_PROXY:
+            ydl_opts['proxy'] = Config.HTTP_PROXY
+        if youtube_dl_username:
+            ydl_opts['username'] = youtube_dl_username
+        if youtube_dl_password:
+            ydl_opts['password'] = youtube_dl_password
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(youtube_link, download=False)
+            video_id = info_dict.get('id')
+            title = info_dict.get('title')
+            duration = info_dict.get("duration", 0)
+
+            if title and video_id:
+                ydl.download([youtube_link])
+                await callback_query.message.edit_text("**Uploading video...**")
+
+                video_filename = f"downloaded_video_{video_id}.mp4"
+                if os.path.exists(video_filename):
+                    await client.send_video(
+                        callback_query.message.chat.id,
+                        video=open(video_filename, 'rb'),
+                        caption=title,
+                        duration=duration
+                    )
+                    os.remove(video_filename)
+
+                await callback_query.message.edit_text("✅ **Successfully Uploaded!**")
+            else:
+                logging.error("No video streams found.")
+                await callback_query.message.edit_text("❌ Error: No downloadable video found.")
+
+    except yt_dlp.utils.DownloadError as e:
+        logging.exception("Error downloading YouTube video: %s", e)
+        await callback_query.message.edit_text("❌ Error: The video is unavailable. It may have been removed or is restricted.")
+    except Exception as e:
+        logging.exception("Error processing YouTube link: %s", e)
+        await callback_query.message.edit_text("❌ Error: Failed to process the YouTube link. Please try again later.")
